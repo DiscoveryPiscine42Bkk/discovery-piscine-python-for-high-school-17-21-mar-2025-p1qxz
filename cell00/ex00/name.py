5db30ceeaf2576e8939b1e9f50c7be2574f91f10
Pimnara Pluemyat <ppluemya@z2t12c4.42bangkok.com>
2025-03-18 09:53:19 +0700

    complete

---
+++ cell00/ex00/name.py
@@ -0,0 +1,3 @@
+first_name="Pimnara"
+last_name="Pluemyat"
+print(first_name,last_name)
---
+++ cell00/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Pimnara" 
+last_name = "Pluemyat"
+whole_name = first_name + " " + last_name
+print(whole_name)
